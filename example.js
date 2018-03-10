760be364e5ea259638dc37c87f3e4c85fca6072b
Hugo Manrique <hugmanrique@gmail.com>
2018-03-10 23:32:47 +0100

    Disable console warning

--- example.js
+++ example.js
@@ -3,6 +3,8 @@ const Server = require('./dist/').default;
 const port = 5000;
 const server = new Server();
 
+/* eslint-disable no-console */
+
 server.listen(port).then(() => {
   console.log(`⚡ Listening on *:${port}`);
 });
